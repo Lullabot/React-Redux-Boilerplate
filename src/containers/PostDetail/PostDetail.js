c7200a8275042ab2ec691c4af105b6b05e2467c0
John Hannah <john@friendlymachine.net>
2016-10-24 17:54:14 -0400

    add react-document-meta and implementation

--- src/containers/PostDetail/PostDetail.js
+++ src/containers/PostDetail/PostDetail.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import DocumentMeta from 'react-document-meta';
 import find from 'lodash/find';
 import Header from '../../components/Header/Header';
 // Import can't be in conditional so use require.
@@ -9,9 +10,19 @@ if (process.env.WEBPACK) {
 }
 
 export const PostDetail = (props) => {
-  const post = find(props.posts.items, { id: parseInt(props.params.postID) });
+  const postID = parseInt(props.params.postID);
+  const meta = {
+    title: 'Post Detail Page',
+    description: 'Put the description here!',
+    canonical: `http://localhost:3000/post-detail/${postID}`,
+    meta: {
+      charset: 'utf-8'
+    }
+  };
+  const post = find(props.posts.items, { id: postID });
   return (
     <div className="page page-post">
+      <DocumentMeta {...meta} />
       <Header />
       <h1 className="page-post__post-title">{post.title}</h1>
       <p>{post.body}</p>
