e67c118c50200c77784d9dc90343a17e138457b9
John Hannah <john@friendlymachine.net>
2016-10-26 15:13:57 -0400

    add async data to redux preload, swap meta tag solution

--- src/containers/PostDetail/PostDetail.js
+++ src/containers/PostDetail/PostDetail.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import DocumentMeta from 'react-document-meta';
+import Meta from 'react-helmet';
 import find from 'lodash/find';
 import Header from '../../components/Header/Header';
 // Import can't be in conditional so use require.
@@ -11,18 +11,32 @@ if (process.env.WEBPACK) {
 
 export const PostDetail = (props) => {
   const postID = parseInt(props.params.postID);
-  const meta = {
+  const head = {
     title: 'Post Detail Page',
-    description: 'Put the description here!',
-    canonical: `http://localhost:3000/post-detail/${postID}`,
-    meta: {
-      charset: 'utf-8'
-    }
+    link: [
+      {
+        rel: 'canonical',
+        href: `http://localhost:3000/post-detail/${postID}`
+      }
+    ],
+    meta: [
+      {
+        charset: 'utf-8'
+      },
+      {
+        name: 'description', content: 'Put the description here!'
+      }
+    ]
   };
   const post = find(props.posts.items, { id: postID });
   return (
     <div className="page page-post">
-      <DocumentMeta {...meta} />
+      <Meta
+        title={head.title}
+        description={head.description}
+        link={head.link}
+        meta={head.meta}
+      />
       <Header />
       <h1 className="page-post__post-title">{post.title}</h1>
       <p>{post.body}</p>
--- src/middleware.js
+++ src/middleware.js
@@ -3,9 +3,11 @@ import { renderToString } from 'react-dom/server';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { match, Router } from 'react-router';
-import DocumentMeta from 'react-document-meta';
+import Meta from 'react-helmet';
 import reducers from './reducers';
 import routes from './routes';
+import api from './lib/api';
+import { receivePosts } from './actions';
 
 const store = createStore(reducers);
 
@@ -29,27 +31,38 @@ export default (req, res) => {
         `);
       }
       else if (process.env.NODE_ENV === 'production') {
-        const preloadedState = store.getState();
-        res.status(200).send(`
-          <html>
-            <head>
-              <link rel='stylesheet' href='bundle.css'>
-              ${DocumentMeta.renderAsReact()}
-            </head>
-            <body>
-              <div id='app'>${renderToString(
-                <Provider store={store}>
-                  <Router {...renderProps} />
-                </Provider>
-              )}
-              </div>
-              <script>
-                window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
-              </script>
-              <script src='bundle.js'></script>
-            </body>
-          </html>
-        `);
+        api('https://jsonplaceholder.typicode.com/posts')
+          .then(
+            json => store.dispatch(receivePosts(json)),
+          )
+          .then(() => {
+            const preloadedState = store.getState();
+            const html = renderToString(
+              <Provider store={store}>
+                <Router {...renderProps} />
+              </Provider>
+            );
+            const head = Meta.rewind();
+            res.status(200).send(`
+              <html>
+                <head>
+                  ${head.title.toString()}
+                  ${head.meta.toString()}
+                  ${head.link.toString()}
+                  <link rel='stylesheet' href='bundle.css'>
+                </head>
+                <body>
+                  <div id='app'>${html}
+                  </div>
+                  <script>
+                    window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)}
+                  </script>
+                  <script src='bundle.js'></script>
+                </body>
+              </html>
+            `);
+            return;
+          });
       }
     }
     else {
